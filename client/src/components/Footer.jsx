611db06384a2aa64badc0d8deae7cc4f87c17081
samuelhan713 <samuelhan.s713@gmail.com>
2024-01-10 17:20:08 -0500

    fixed footer links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,18 @@
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+    const navigate = useNavigate();
     return (
         <div className="w-full relative mt-14 flex flex-col items-center text-center">
             <div className="flex flex-col md:flex-row items-center text-base font-normal">
-                <a className="mx-2 opacity-50 transition-opacity duration-200 ease-in-out hover:opacity-100" href="/">Home</a>
-                <a className="mx-2 opacity-50 transition-opacity duration-200 ease-in-out hover:opacity-100" href="/about">About</a>
-                <a className="mx-2 opacity-50 transition-opacity duration-200 ease-in-out hover:opacity-100" href="/experience">Experience</a>
-                <a className="mx-2 opacity-50 transition-opacity duration-200 ease-in-out hover:opacity-100" href="/projects">Projects</a>
-                <a className="mx-2 opacity-50 transition-opacity duration-200 ease-in-out hover:opacity-100" href="/allMessages">Notes</a>
+                <a className="mx-2 opacity-50 transition-opacity duration-200 ease-in-out hover:opacity-100 cursor-pointer" onClick={() => navigate('/')}>Home</a>
+                <a className="mx-2 opacity-50 transition-opacity duration-200 ease-in-out hover:opacity-100 cursor-pointer" onClick={() => navigate('/about')}>About</a>
+                <a className="mx-2 opacity-50 transition-opacity duration-200 ease-in-out hover:opacity-100 cursor-pointer" onClick={() => navigate('/experience')}>Experience</a>
+                <a className="mx-2 opacity-50 transition-opacity duration-200 ease-in-out hover:opacity-100 cursor-pointer" onClick={() => navigate('/projects')}>Projects</a>
+                <a className="mx-2 opacity-50 transition-opacity duration-200 ease-in-out hover:opacity-100 cursor-pointer" onClick={() => navigate('/allMessages')}>Notes</a>
             </div>
             <ul className="bg-gray-100 flex items-center justify-center p-1 rounded-3xl shadow-sm mt-5">
                 <a className="p-2" target="_blank" href="https://github.com/samuelhan713">
