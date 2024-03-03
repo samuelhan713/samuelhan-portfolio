2fcc0d8e4cc8da50f94918272cd50319079c4c4c
samuelhan713 <samuelhan.s713@gmail.com>
2024-03-02 22:21:39 -0500

    changed project section

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -2,6 +2,9 @@ import { FaEarthAmericas } from "react-icons/fa6";
 import { IoIosJournal } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 import { CiBitcoin } from "react-icons/ci";
+import { RiMentalHealthLine } from "react-icons/ri";
+import { MdOutlineHealthAndSafety } from "react-icons/md";
+import { MdOutlinePeople } from "react-icons/md";
 
 const Projects = () => {
     const navigate = useNavigate();
@@ -10,6 +13,69 @@ const Projects = () => {
             <h1 className="text-4xl">Projects</h1>
             {/* <div className="text-center mt-4 flex items-center"> */}
             <div className="mt-4 lg:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                <a href="https://github.com/samuelhan713/Vital" target="_blank" class="p-5 m-2 flex flex-col items-start bg-gray-50 rounded-3xl cursor-pointer hover:bg-gray-100 text-left hover:scale-105 duration-300">
+                    <div size="40" color="E0E6EB" class="go1550876186">
+                        <span name="Shape35" color="2D3A46" size="20" role="img" class="go2599496350">
+                            <MdOutlineHealthAndSafety />
+                        </span>
+                    </div>
+                    <h3 class="text-2xl mt-4 font-medium">Vital</h3>
+                    <p class="mt-2 opacity-50 font-light text-base">
+                        Get recommendations about essential dietary supplements with machine learning!
+                    </p>
+                    <div className="flex justify-between mt-4">
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">MERN</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">NLP</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Redux</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Material UI</span>
+                    </div>
+                    <div className="flex justify-center items-center mt-10 ml-80">
+                        <MdOutlinePeople />
+                        <span class="text-sm">3 developers</span>
+                    </div>
+                </a>
+                <a href="https://github.com/sms021019/mental-health-tracker" target="_blank" class="p-5 m-2 flex flex-col items-start bg-gray-50 rounded-3xl cursor-pointer hover:bg-gray-100 text-left hover:scale-105 duration-300">
+                    <div size="40" color="E0E6EB" class="go1550876186">
+                        <span name="Shape35" color="2D3A46" size="20" role="img" class="go2599496350">
+                            <RiMentalHealthLine />
+                        </span>
+                    </div>
+                    <h3 class="text-2xl mt-4 font-medium">MindBloom.ai</h3>
+                    <p class="mt-2 opacity-50 font-light text-base">
+                        Track and improve your mental health with the power of AI!
+                    </p>
+                    <div className="flex justify-between mt-4">
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">MERN</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">NLP</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Redux</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Material UI</span>
+                    </div>
+                    <div className="flex justify-center items-center mt-10 ml-80">
+                        <MdOutlinePeople />
+                        <span class="text-sm">4 developers</span>
+                    </div>
+                </a>
+                <a href="https://mapbook-f381d1faf354.herokuapp.com/" target="_blank" class="p-5 m-2 flex flex-col items-start bg-gray-50 rounded-3xl cursor-pointer hover:bg-gray-100 text-left hover:scale-105 duration-300">
+                    <div size="40" color="E0E6EB" class="go1550876186">
+                        <span name="Shape35" color="2D3A46" size="20" role="img" class="go2599496350">
+                            <FaEarthAmericas />
+                        </span>
+                    </div>
+                    <h3 class="text-2xl mt-4 font-medium">MapBook</h3>
+                    <p class="mt-2 opacity-50 font-light text-base">
+                        Create, edit, and share maps with your friends or strangers!
+                    </p>
+                    <div className="flex justify-between mt-4">
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">MERN</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Redux</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Firebase</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Heroku</span>
+                    </div>
+                    <div className="flex justify-center items-center mt-10 ml-80">
+                        <MdOutlinePeople />
+                        <span class="text-sm">4 developers</span>
+                    </div>
+                </a>
                 <a href="https://github.com/samuelhan713/r-CryptoCurrency_Data_Analysis" target="_blank" class="p-5 m-2 flex flex-col items-start bg-gray-50 rounded-3xl cursor-pointer hover:bg-gray-100 text-left hover:scale-105 duration-300">
                     <div size="40" color="E0E6EB" class="go1550876186">
                         <span name="Shape35" color="2D3A46" size="20" role="img" class="go2599496350">
@@ -28,23 +94,6 @@ const Projects = () => {
                     <div className="flex justify-between">
                         <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1 mt-2">NLTK</span>
                         <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1 mt-2">Machine Learning</span>
-                    </div>
-                </a>
-                <a href="https://mapbook-f381d1faf354.herokuapp.com/" target="_blank" class="p-5 m-2 flex flex-col items-start bg-gray-50 rounded-3xl cursor-pointer hover:bg-gray-100 text-left hover:scale-105 duration-300">
-                    <div size="40" color="E0E6EB" class="go1550876186">
-                        <span name="Shape35" color="2D3A46" size="20" role="img" class="go2599496350">
-                            <FaEarthAmericas />
-                        </span>
-                    </div>
-                    <h3 class="text-2xl mt-4 font-medium">MapBook</h3>
-                    <p class="mt-2 opacity-50 font-light text-base">
-                        Create, edit, and share maps with your friends or strangers!
-                    </p>
-                    <div className="flex justify-between mt-4">
-                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">MERN</span>
-                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Redux</span>
-                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Firebase</span>
-                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Heroku</span>
                     </div>
                 </a>
             </div>
--- client/src/components/ProjectsPage.jsx
+++ client/src/components/ProjectsPage.jsx
@@ -3,12 +3,57 @@ import { IoIosJournal } from "react-icons/io";
 import Footer from "./Footer";
 import { CiChat1 } from "react-icons/ci";
 import { CiBitcoin } from "react-icons/ci";
+import { RiMentalHealthLine } from "react-icons/ri";
+import { MdOutlinePeople } from "react-icons/md";
+import { MdOutlineHealthAndSafety } from "react-icons/md";
 
 const ProjectsPage = () => {
     return (
         <div className="mt-10 flex flex-col items-center">
             <h1 className="text-5xl">Projects</h1>
             <div className="mt-10 lg:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                <a href="https://github.com/samuelhan713/Vital" target="_blank" class="p-5 m-2 flex flex-col items-start bg-gray-50 rounded-3xl cursor-pointer hover:bg-gray-100 text-left hover:scale-105 duration-300">
+                    <div size="40" color="E0E6EB" class="go1550876186">
+                        <span name="Shape35" color="2D3A46" size="20" role="img" class="go2599496350">
+                            <MdOutlineHealthAndSafety />
+                        </span>
+                    </div>
+                    <h3 class="text-2xl mt-4 font-medium">Vital</h3>
+                    <p class="mt-2 opacity-50 font-light text-base">
+                        Get recommendations about essential dietary supplements with machine learning!
+                    </p>
+                    <div className="flex justify-between mt-4">
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">MERN</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">NLP</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Redux</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Material UI</span>
+                    </div>
+                    <div className="flex justify-center items-center mt-10 ml-80">
+                        <MdOutlinePeople />
+                        <span class="text-sm">3 developers</span>
+                    </div>
+                </a>
+                <a href="https://github.com/sms021019/mental-health-tracker" target="_blank" class="p-5 m-2 flex flex-col items-start bg-gray-50 rounded-3xl cursor-pointer hover:bg-gray-100 text-left hover:scale-105 duration-300">
+                    <div size="40" color="E0E6EB" class="go1550876186">
+                        <span name="Shape35" color="2D3A46" size="20" role="img" class="go2599496350">
+                            <RiMentalHealthLine />
+                        </span>
+                    </div>
+                    <h3 class="text-2xl mt-4 font-medium">MindBloom.ai</h3>
+                    <p class="mt-2 opacity-50 font-light text-base">
+                        Track and improve your mental health with the power of AI!
+                    </p>
+                    <div className="flex justify-between mt-4">
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">MERN</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">NLP</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Redux</span>
+                        <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Material UI</span>
+                    </div>
+                    <div className="flex justify-center items-center mt-10 ml-80">
+                        <MdOutlinePeople />
+                        <span class="text-sm">4 developers</span>
+                    </div>
+                </a>
                 <a href="https://github.com/samuelhan713/r-CryptoCurrency_Data_Analysis" target="_blank" class="p-5 m-2 flex flex-col items-start bg-gray-50 rounded-3xl cursor-pointer hover:bg-gray-100 text-left hover:scale-105 duration-300">
                     <div size="40" color="E0E6EB" class="go1550876186">
                         <span name="Shape35" color="2D3A46" size="20" role="img" class="go2599496350">
@@ -49,7 +94,10 @@ const ProjectsPage = () => {
                         <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Firebase</span>
                         <span class="text-sm py-2 px-4 bg-gray-200 rounded-3xl mr-1">Heroku</span>
                     </div>
-
+                    <div className="flex justify-center items-center mt-10 ml-80">
+                        <MdOutlinePeople />
+                        <span class="text-sm">4 developers</span>
+                    </div>
                 </a>
                 <a href="https://main--subtle-biscotti-65378e.netlify.app/" target="_blank" class="p-5 m-2 flex flex-col items-start bg-gray-50 rounded-3xl cursor-pointer hover:bg-gray-100 text-left hover:scale-105 duration-300">
                     <div size="40" color="E0E6EB" class="go1550876186 w-full">
